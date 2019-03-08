390c99592db3b98916b94b5a8bc9fe3b2fc89a33
MTeuber <32986982+MTeuber@users.noreply.github.com>
2019-03-08 09:47:57 +0100

    imagesInYaml

--- lib/pages/create_user.dart
+++ lib/pages/create_user.dart
@@ -8,10 +8,31 @@ class CreateUserPage extends StatefulWidget {
 }
 
 class _CreateUserState extends State<CreateUserPage> {
-  
+  final Map<String, dynamic> _formData = {
+    'name': null,
+    'password': null,
+    'image': 'assets/images/NoPicMale.png',
+    'Gender': null,
+    'Bio': null,
+    'City': null,
+    'CreatedDate': null,
+  };
+
+  Widget _buildNameTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Brugernavn',
+        labelStyle: TextStyle(fontSize: 24)
+      ),
+    );
+  }
+
   @override
-  Widget build(BuildContext context)
-  {
-    return ListView(children: <Widget>[Container()],);
+  Widget build(BuildContext context) {
+    return Container(color: Theme.of(context).backgroundColor,
+      child: ListView(
+        children: <Widget>[],
+      ),
+    );
   }
 }
